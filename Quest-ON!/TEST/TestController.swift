18381ac3a3b6f42876a4239a7466369618c8d2ae
Nandapp <nanda.putra.perkasa@gmail.com>
2020-04-30 15:56:53 +0700

    test class file

---
+++ Quest-ON!/TEST/TestController.swift
@@ -0,0 +1,30 @@
+//
+//  TestController.swift
+//  Quest-ON!
+//
+//  Created by Nanda Putra Perkasa on 30/04/20.
+//  Copyright © 2020 Nanda Putra Perkasa. All rights reserved.
+//
+
+import UIKit
+
+class TestController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
